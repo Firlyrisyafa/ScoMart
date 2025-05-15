d99a123dfad75375846b5aafb06dbcf5e5616b6d
Firlyrisyafa <firlyrisyafaanindita@gmail.com>
2025-05-15 19:52:53 +0700

    modify

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
     <title>@yield('title', 'Admin Dashboard')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
 </head>
 <body class="bg-white font-sans antialiased">
     <div class="min-h-screen">
